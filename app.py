bbefdd8c3b43887dd59b894495b5515e9bf20ec7
pythus <isakulovDev@gmail.com>
2021-09-02 16:08:03 +0500

    First and last commit(I think)

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template , request
+import pyshorteners
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+	return render_template('index.html')
+
+@app.route('/url',methods=['GET','POST'])
+def result():
+	key = request.form.get('text')
+	shortener = pyshorteners.Shortener()
+	x = shortener.tinyurl.short(key)
+	return render_template('index.html',result=x)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
