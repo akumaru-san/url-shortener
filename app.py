113d61fd1d59cd962647ffba1f51dfd968772003
Akumaru <87922505+akumaru-san@users.noreply.github.com>
2022-05-24 06:46:36 +0500

    Cleaned and changed main file.

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
-from flask import Flask, render_template , request
-import pyshorteners
-
-app = Flask(__name__)
-
-@app.route('/',methods=['GET','POST'])
-def home():
-	return render_template('index.html')
-
-@app.route('/url',methods=['GET','POST'])
-def result():
-	key = request.form.get('text')
-	shortener = pyshorteners.Shortener()
-	x = shortener.tinyurl.short(key)
-	return render_template('index.html',result=x)
-
+from config import app
+import views
 
 if __name__ == '__main__':
-	app.run(port=80)
+	# Debug mode
+	# app.run(port=8080, debug=True)
+	# Production
+	app.run(debug=False)
